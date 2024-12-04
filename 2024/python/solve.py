d6d7c634b4b9ece34c81eabe1674360647b63865
Marijn van Vliet <w.m.vanvliet@gmail.com>
2024-12-04 10:31:53 +0200

    solve day4

---
+++ 2024/python/solve.py
@@ -0,0 +1,44 @@
+import re
+
+lines = open("day4.txt").read().strip().split("\n")
+n_lines = len(lines)
+
+n_xmas = 0
+for line in lines:
+    n_xmas += len(re.findall("XMAS", line))
+    n_xmas += len(re.findall("SAMX", line))
+
+lines2 = ["".join(line) for line in zip(*[list(line) for line in lines])]
+for line in lines2:
+    n_xmas += len(re.findall("XMAS", line))
+    n_xmas += len(re.findall("SAMX", line))
+
+lines3 = [
+    "".join([lines[i + j][i] for i in range(max(-j, 0), min(n_lines - j, n_lines))])
+    for j in range(-n_lines, n_lines)
+]
+for line in lines3:
+    n_xmas += len(re.findall("XMAS", line))
+    n_xmas += len(re.findall("SAMX", line))
+
+lines4 = [
+    "".join(
+        [lines[i + j][-(i + 1)] for i in range(max(-j, 0), min(n_lines - j, n_lines))]
+    )
+    for j in range(-n_lines, n_lines)
+]
+for line in lines4:
+    n_xmas += len(re.findall("XMAS", line))
+    n_xmas += len(re.findall("SAMX", line))
+
+print("Day 4, part 1:", n_xmas)
+
+
+n_xmas = 0
+for y in range(1, n_lines - 1):
+    for x in range(1, n_lines - 1):
+        mas1 = "".join([lines[y + i][x + i] for i in range(-1, 2)])
+        mas2 = "".join([lines[y - i][x + i] for i in range(-1, 2)])
+        if (mas1 == "MAS" or mas1 == "SAM") and (mas2 == "MAS" or mas2 == "SAM"):
+            n_xmas += 1
+print("Day 4, part 2:", n_xmas)
