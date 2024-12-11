bb506f33978745d0317d5aadd036291e4151cc57
Marijn van Vliet <w.m.vanvliet@gmail.com>
2024-12-11 07:43:25 +0200

    2024 day 11

--- 2024/python/solve.py
+++ 2024/python/solve.py
@@ -587,3 +587,28 @@ for _ in range(9):
 
 print("Day 10, part 1:", sum(len(grid[p][1]) for p in starting_points))
 print("Day 10, part 2:", sum(grid[p][2] for p in starting_points))
+
+##
+from collections import Counter
+stones = Counter([int(x) for x in open("day11.txt").read().strip().split()])
+
+def count_stones(stones, n):
+    for i in range(n):
+        new_stones = dict()
+        for stone, num in stones.items():
+            if stone == 0:
+                new_stones[1] = new_stones.get(1, 0) + num
+                continue
+            stone_str = str(stone)
+            if len(stone_str) % 2 == 0:
+                first_half = int(stone_str[:len(stone_str) // 2])
+                second_half = int(stone_str[len(stone_str) // 2:])
+                new_stones[first_half] = new_stones.get(first_half, 0) + num
+                new_stones[second_half] = new_stones.get(second_half, 0) + num
+            else:
+                prod = stone * 2024
+                new_stones[prod] = new_stones.get(prod, 0) + num
+        stones = new_stones
+    return sum(stones.values())
+print("Day 11, part 1:", count_stones(stones, 25))
+print("Day 11, part 2:", count_stones(stones, 75))
