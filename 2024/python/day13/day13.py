54790dd314f47bda8569e5f7aa5d6e51bee29641
Marijn van Vliet <w.m.vanvliet@gmail.com>
2024-12-29 13:48:41 +0200

    2024 day 13

--- 2024/python/day13/day13.py
+++ 2024/python/day13/day13.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 
 part1 = 0
+part2 = 0
 with open("day13.txt") as f:
     try:
         while True:
@@ -10,15 +11,17 @@ with open("day13.txt") as f:
             x, y = re.match(r"Button B: X([\+-]\d+), Y([\+-]\d+)", next(f)).groups()
             B = (int(x), int(y))
             x, y = re.match(r"Prize: X=(\d+), Y=(\d+)", next(f)).groups()
-            #prize = (int(x) + 10000000000000, int(y) + 10000000000000)
             prize = (int(x), int(y))
             next(f)
+
             n_a, n_b = np.linalg.solve(np.vstack((A, B)).T, np.array(prize))
-            if abs(n_a - round(n_a)) < 1e-1 and abs(n_b - round(n_b)) < 1e-1:
-                print(n_a, n_b, int(round(n_a * 3 + n_b)))
+            if abs(n_a - round(n_a)) < 1e-3 and abs(n_b - round(n_b)) < 1e-3:
                 part1 += int(round(n_a * 3 + n_b))
-            else:
-                print(n_a, n_b, "no solution")
+
+            n_a, n_b = np.linalg.solve(np.vstack((A, B)).T, np.array(prize, dtype="int64") + 10000000000000)
+            if abs(n_a - round(n_a)) < 1e-3 and abs(n_b - round(n_b)) < 1e-3:
+                part2 += int(round(n_a * 3 + n_b))
     except StopIteration:
         pass
 print("part 1:", part1)
+print("part 2:", part2)
